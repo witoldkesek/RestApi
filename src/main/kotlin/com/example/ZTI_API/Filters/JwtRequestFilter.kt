4396f695cab7e5b8961ca14af5d2d7df2f037b20
Wiciu <witoldkesek@gmail.com>
2021-05-29 03:34:07 +0200

    Authorization and registeration work perfectly

---
+++ src/main/kotlin/com/example/ZTI_API/Filters/JwtRequestFilter.kt
@@ -0,0 +1,46 @@
+package com.example.ZTI_API.Filters
+
+import com.example.ZTI_API.MyUserDetailsService
+import com.example.ZTI_API.Utils.JwtUtil
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class JwtRequestFilter : OncePerRequestFilter() {
+    @Autowired
+    private val userDetailsService: MyUserDetailsService? = null
+
+    @Autowired
+    private val jwtUtil: JwtUtil? = null
+
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+        val authorizationHeader = request.getHeader("Authorization")
+        var username: String? = null
+        var jwt: String? = null
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7)
+            username = jwtUtil?.extractUsername(jwt)
+        }
+        if (username != null && SecurityContextHolder.getContext().authentication == null) {
+            val userDetails = userDetailsService!!.loadUserByUsername(username)
+            if (jwtUtil!!.validateToken(jwt, userDetails)) {
+                val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.authorities
+                )
+                usernamePasswordAuthenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+                SecurityContextHolder.getContext().authentication = usernamePasswordAuthenticationToken
+            }
+        }
+        chain.doFilter(request, response)
+    }
+}
